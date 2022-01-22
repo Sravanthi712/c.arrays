b251f1bbee9d8aeb1557f55bae87d86fd268f77f
Sravanthi712 <96292238+Sravanthi712@users.noreply.github.com>
2022-01-22 12:46:30 +0530

    Add files via upload

---
+++ evennumbersarray.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+	int size;
+	scanf("%d",&size);
+	int i,num[size];
+	for(i=0;i<size;i++)
+	{
+	
+	scanf("%d",&num[i]);
+}
+for(i=0;i<size;i++)
+{
+	if(num[i]%2==0)
+
+	printf("%d\n",num[i]);
+
+else
+
+
+printf("no even numbers");
+}
+}
